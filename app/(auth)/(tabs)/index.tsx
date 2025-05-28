e2482cbc4672e7fcba69a876d803e832e32ff8ff
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2025-05-28 13:42:31 +1000

    fixed signIn form bug

--- app/(auth)/(tabs)/index.tsx
+++ app/(auth)/(tabs)/index.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
 
-const index = () => {
+const Index = () => {
+  const { signOut } = useAuth();
   return (
     <View>
-      <Text>index</Text>
+      <Button onPress={() => signOut()} title="Loggout" />
+
+      <Text>Index</Text>
     </View>
   );
 };
 
-export default index;
+export default Index;
 
 const styles = StyleSheet.create({});
